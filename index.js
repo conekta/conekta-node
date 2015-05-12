47304681e71a3e7211f6cd76b4a9f45734e51331
Julian Ceballos <julian@pikhub.com>
2015-05-12 09:28:24 -0500

    update conekta node

---
+++ index.js
@@ -0,0 +1,1 @@
+var conekta = require('lib/conekta.js');
---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require("assert"),
+    request = require('request');
+
+describe('conekta', function() {
+    describe('Charge', function() {
+        it('create', function() {
+            assert(true, true);
+        });
+    });
+});
