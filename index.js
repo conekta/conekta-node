c09af15799ac39758e1141e1244f0052fad3d3e2
Julian Ceballos <julian@pikhub.com>
2015-05-13 10:28:24 -0500

    wrapper update

--- index.js
+++ index.js
@@ -1,1 +1,261 @@
-var conekta = require('lib/conekta.js');
+//Basics
+var _ = require('underscore'),
+    base64 = require('./lib/base64.js'),
+    pkg = require('./package');
+
+//Global
+var API_VERSION = '1.0.0',
+    API_BASE = 'https://api.conekta.io',
+    HEADERS = {
+        'Accept': ['application/vnd.conekta-v', API_VERSION ,'+json'].join(''),
+        'Content-Type': 'application/json'
+    };
+
+var Requestor = function(params) {
+    this.apiUrl = API_BASE;
+    this.headers = {
+        bindings_version: ['Conekta::', pkg.version].join(''),
+        lang: 'node',
+        lang_version: process.version,
+        publisher: 'conekta',
+        uname: ''
+    };
+    this.request = function(opts) {
+        HEADERS['Conekta-Client-User-Agent'] = this.headers;
+        HEADERS['User-Agent'] = 'Conekta/v1 NodeBindings/' + ['Conekta::', pkg.version].join('');
+        HEADERS['Accept-Language'] = Conekta.locale;
+        HEADERS['Content-Type'] = 'application/json';
+        HEADERS['Authorization'] = ['Basic ', base64.encode(Conekta.api_key), ':'].join('');
+
+        var request = require('request'),
+            fs = require('fs');
+
+        var options = {
+            url: opts.url,
+            headers: HEADERS
+        };
+
+        if (opts.method == 'get') options['qs'] = opts.data;
+        if (opts.method == 'post') {
+            options['form'] = JSON.stringify(opts.data);
+        } else {
+            options['form'] = opts.data;
+        }
+
+        request[opts.method](options, function(err, req, res) {
+            if (req.statusCode != 200 && req.statusCode != 201) {
+                opts.error(JSON.parse(res));
+            } else {
+                opts.success(JSON.parse(res));
+            }
+        });
+    }
+};
+
+var Resource = function(instance) {
+    return _.extend({
+        classUrl: '',
+        get: function(opts, id) {
+            var uri = this.classUrl;
+            if (id) {
+                uri += '/' + id;
+            }
+
+            var requestor = new Requestor();
+            requestor.request({
+                method: 'get',
+                url: [Conekta.apiBase, uri].join(''),
+                success: opts.success,
+                error: opts.error
+            });
+        },
+        post: function(opts, id) {
+            var uri = this.classUrl;
+            if (id) {
+                uri += '/' + id;
+            }
+
+            var requestor = new Requestor();
+            requestor.request({
+                method: 'post',
+                url: [Conekta.apiBase, uri].join(''),
+                data: JSON.stringify(opts.data),
+                //data: opts.data,
+                success: opts.success,
+                error: opts.error
+            });
+        },
+        put: function(opts, id) {
+            var uri = this.classUrl;
+            if (id) {
+                uri += '/' + id;
+            }
+
+            var requestor = new Requestor();
+            requestor.request({
+                method: 'put',
+                url: [Conekta.apiBase, uri].join(''),
+                data: opts.data,
+                success: opts.success,
+                error: opts.error
+            });
+        },
+        del: function(opts, id) {
+            var uri = this.classUrl;
+            if (id) {
+                uri += '/' + id;
+            }
+
+            var requestor = new Requestor();
+            requestor.request({
+                method: 'del',
+                url: [Conekta.apiBase, uri].join(''),
+                data: opts.data,
+                success: opts.success,
+                error: opts.error
+            });
+        },
+        custom: function() {}
+    }, instance);
+}
+
+var Charge = new Resource({
+    classUrl: '/charges',
+    find: function(id, success, error) {
+        this.get({
+            success: success,
+            error: error
+        }, id);
+    },
+    where: function(data, success, error) {
+        this.get({
+            data: data,
+            success: success,
+            error: error
+        });
+    },
+    create: function(data, success, error) {
+        this.post({
+            data: data,
+            success: success,
+            error: error
+        });
+    },
+    refund: function(id, data, success, error) {
+        this.custom = function(opts, id) {
+            var uri = [this.classUrl, id, 'refund'].join('/');
+
+            var requestor = new Requestor();
+            requestor.request({
+                method: 'post',
+                url: [Conekta.apiBase, uri].join(''),
+                data: opts.data,
+                success: opts.success,
+                error: opts.error
+            });
+        }
+        this.custom({
+            data: data,
+            success: success,
+            error: error
+        }, id);
+    },
+    capture: function(id, success, error) {
+        this.custom = function(opts, id) {
+            var uri = [this.classUrl, id, 'capture'].join('/');
+
+            var requestor = new Requestor();
+            requestor.request({
+                method: 'post',
+                url: [Conekta.apiBase, uri].join(''),
+                data: opts.data,
+                success: opts.success,
+                error: opts.error
+            });
+        }
+        this.custom({
+            data: {},
+            success: success,
+            error: error
+        }, id);
+    }
+});
+
+var Plan = new Resource({
+    classUrl: '/plans',
+    where: function(data, success, error) {
+        this.get({
+            data: data,
+            success: success,
+            error: error
+        });
+    },
+    create: function(data, success, error) {
+        this.post({
+            data: data,
+            success: success,
+            error: error
+        });
+    },
+    update: function(id, data, success, error) {
+        this.put({
+            data: data,
+            success: success,
+            error: error
+        }, id);
+    },
+    delete: function(id, success, error) {
+        this.del({
+            data: {},
+            success: success,
+            error: error
+        }, id);
+    }
+});
+
+var Event = new Resource({
+    classUrl: '/events',
+    where: function(data, success, error) {
+        this.get({
+            data: data,
+            success: success,
+            error: error
+        });
+    }
+});
+
+var Customer = new Resource({
+    classUrl: '/customers',
+    create: function(data, success, error) {
+        this.post({
+            data: data,
+            success: success,
+            error: error
+        });
+    },
+    update: function(id, data, success, error) {
+        this.put({
+            data: data,
+            success: success,
+            error: error
+        }, id);
+    },
+    delete: function(id, success, error) {
+        this.del({
+            data: {},
+            success: success,
+            error: error
+        }, id);
+    }
+});
+
+module.exports = Conekta = {
+    api_key: '',
+    apiBase: API_BASE,
+    apiVersion: API_VERSION,
+    locale: 'en',
+    Charge: Charge,
+    Event: Event,
+    Customer: Customer,
+    Plan: Plan
+};
--- test/test.js
+++ test/test.js
@@ -1,10 +1,296 @@
-var assert = require("assert"),
-    request = require('request');
+var assert = require('assert'),
+    conekta = require('../index.js');
 
-describe('conekta', function() {
-    describe('Charge', function() {
-        it('create', function() {
-            assert(true, true);
+const LOCALE = 'es',
+    API_KEY = 'key_eYvWV7gSDkNYXsmr';
+
+var charge = '',
+    chargeCapture = '',
+    amount = 50000,
+    plan = 'jul-plan';
+/*
+describe('Charge', function() {
+
+    describe('create with card', function() {
+        it('should return object instance with id attribute', function(done) {
+            this.timeout(60000);
+            conekta.api_key = API_KEY;
+            conekta.locale = LOCALE;
+            conekta.Charge.create({
+                description: 'Stogies',
+                amount: amount,
+                currency: 'MXN',
+                reference_id: '9839-wolf_pack',
+                card: 'tok_test_visa_4242',
+                details: {
+                    email: 'logan@x-men.org'
+                },
+                capture: false
+            }, function(res) {
+                chargeCapture = res.id;
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
         });
     });
+
+    describe('create with card montly installments', function() {
+        it('should return object instance with id attribute', function(done) {
+            this.timeout(60000);
+            conekta.api_key = API_KEY;
+            conekta.locale = LOCALE;
+            conekta.Charge.create({
+                description: 'Stogies',
+                amount: amount,
+                currency: 'MXN',
+                reference_id: '9839-wolf_pack',
+                monthly_installments: 3,
+                card: 'tok_test_visa_4242',
+                details: {
+                    email: 'logan@x-men.org'
+                }
+            }, function(res) {
+                charge = res.id;
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });
+
+    describe('create with oxxo', function() {
+        it('should return object instance with id attribute', function(done) {
+            this.timeout(60000);
+            conekta.api_key = API_KEY;
+            conekta.locale = LOCALE;
+            conekta.Charge.create({
+                currency: 'MXN',
+                amount: amount,
+                description: 'Stogies',
+                reference_id: '9839-wolf_pack',
+                cash: {
+                    type: 'oxxo'
+                },
+                details: {
+                    name: 'Wolverine',
+                    email: 'logan@x-men.org',
+                    phone: '403-342-0642'
+                }
+            }, function(res) {
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });
+
+    describe('create with spei', function() {
+        it('should return object instance with id attribute', function(done) {
+            this.timeout(60000);
+            conekta.api_key = API_KEY;
+            conekta.locale = LOCALE;
+            conekta.Charge.create({
+                currency:'MXN',
+                amount: amount,
+                description: 'Stogies',
+                reference_id: '9839-wolf_pack',
+                bank: {
+                    type: 'spei'
+                },
+                details: {
+                    name: 'Wolverine',
+                    email: 'logan@x-men.org',
+                    phone: '403-342-0642'
+                }
+            }, function(res) {
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });
+    
+    describe('find', function() {
+        it('should return object with same id sent before', function(done) {
+            this.timeout(60000);
+            conekta.api_key = API_KEY;
+            conekta.locale = LOCALE;
+            conekta.Charge.find(charge, function(res) {
+                assert(res.id == charge, true);
+                done();
+            });
+        });
+    });
+
+    describe('where', function() {
+        it('should return array', function(done) {
+            this.timeout(60000);
+            conekta.api_key = API_KEY;
+            conekta.locale = LOCALE;
+            conekta.Charge.where({}, function(res) {
+                assert(res instanceof Array, true);
+                done();
+            });
+        });
+    });
+
+    describe('refund', function() {
+        it('should return object instance with id attribute', function(done) {
+            this.timeout(60000);
+            conekta.api_key = API_KEY;
+            conekta.locale = LOCALE;
+            conekta.Charge.refund(charge, {
+                amount: 20000
+            }, function(res) {
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });
+
+    describe('capture', function() {
+        it('should return object instance with id attribute', function(done) {
+            this.timeout(60000);
+            conekta.api_key = API_KEY;
+            conekta.locale = LOCALE;
+            conekta.Charge.capture(chargeCapture, function(res) {
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });
+    
+});
+*/
+describe('Plan', function() {
+
+    describe('create', function() {
+        it('should return instance object with id', function(done) {
+            this.timeout(60000);
+            conekta.api_key = 'key_eYvWV7gSDkNYXsmr';
+            conekta.locale = 'es';
+            conekta.Plan.create({
+                id: plan,
+                name: 'Gold Plan',
+                amount: 10000,
+                currency: 'MXN',
+                interval: 'month',
+                frequency: 1,
+                trial_period_days: 15,
+                expiry_count: 12
+            }, function(res) {
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });
+
+    describe('update', function() {
+        it('should return instance object with id', function(done) {
+            this.timeout(60000);
+            conekta.api_key = 'key_eYvWV7gSDkNYXsmr';
+            conekta.locale = 'es';
+            conekta.Plan.update(plan, {
+                name: 'Gold Plan',
+                amount: 10000,
+                currency: 'MXN',
+                interval: 'month',
+                frequency: 1,
+                trial_period_days: 15,
+                expiry_count: 12
+            }, function(res) {
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });
+
+    describe('where', function() {
+        it('should return array', function(done) {
+            this.timeout(60000);
+            conekta.api_key = 'key_eYvWV7gSDkNYXsmr';
+            conekta.locale = 'es';
+            conekta.Plan.where({}, function(res) {
+                assert(res instanceof Array, true);
+                done();
+            });
+        });
+    });
+
+    describe('delete', function() {
+        it('should return instance object with id', function(done) {
+            this.timeout(60000);
+            conekta.api_key = 'key_eYvWV7gSDkNYXsmr';
+            conekta.locale = 'es';
+            conekta.Plan.delete(plan, function(res) {
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    })
+
+});
+
+describe('Event', function() {
+    describe('where', function() {
+        it('should return array', function(done) {
+            this.timeout(60000);
+            conekta.api_key = 'key_eYvWV7gSDkNYXsmr';
+            conekta.locale = 'es';
+            conekta.Event.where({}, function(res) {
+                assert(res instanceof Array, true);
+                done();
+            });
+        });
+    });
+});
+
+describe('Customer', function() {
+    
+    describe('create', function() {
+        it('should return an object instance with id', function(done) {
+            this.timeout(60000);
+            conekta.api_key = 'key_eYvWV7gSDkNYXsmr';
+            conekta.locale = 'es';
+            conekta.Customer.create({
+                name:'James Howlett',
+                email:'james.howlett@forces.gov',
+                phone:'55-5555-5555',
+                cards: ['tok_test_visa_4242'],
+                plan: 'gold-plan'
+            }, function(res) {
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });
+    /*
+    describe('update', function() {
+        it('should return an object instance with id', function(done) {
+            this.timeout(60000);
+            conekta.api_key = 'key_eYvWV7gSDkNYXsmr';
+            conekta.locale = 'es';
+            conekta.Customer.update('cus_k2D9DxlqdVTagmEd400001', {
+                name: 'Logan',
+                email: 'logan@x-men.org'
+            }, function(res) {
+                console.log(res, '<<<<');
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            }, function(res) {
+                console.log(res);
+            });
+        });
+    });
+
+    describe('delete', function() {
+        it('should return an object instance with id', function(done) {
+            this.timeout(60000);
+            conekta.api_key = 'key_eYvWV7gSDkNYXsmr';
+            conekta.locale = 'es';
+            conekta.Customer.delete('cus_k2D9DxlqdVTagmEd400001', function(res) {
+                console.log(res, '<<<<');
+                assert(res.hasOwnProperty('id'), true);
+                done();
+            });
+        });
+    });*/
+
 });
